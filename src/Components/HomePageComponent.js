bf62d544c9338f4cc86cd77999eaa0a173dc2f7f
JayvonK <jaykhuth@gmail.com>
2024-02-26 09:34:35 -0800

    first commit

---
+++ src/Components/HomePageComponent.js
@@ -0,0 +1,14 @@
+//rafce short hand for react arrow function component export
+import React from 'react'
+
+const HomePageComponent = () => {
+  return (
+    <div>
+      <h1>Hello this is our Home Page</h1>
+    </div>
+  )
+}
+
+export default HomePageComponent
+
+
