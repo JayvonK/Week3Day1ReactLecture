c3f1c574ae5621c83f5e5438aa9716df75fb9738
JayvonK <jaykhuth@gmail.com>
2024-02-26 10:14:50 -0800

    use navigate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePageComponent from './Components/HomePageComponent';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProfilePageComponent from './Components/ProfilePageComponent';
 
 function App() {
   return (
-    <>
-      <HomePageComponent />
-    </>
+      <BrowserRouter>
+      {/* Wrapping our application in browserRouter to enable Routing */}
+
+      <Routes>
+        {/* '/' deteramnds our hompage our are default rendered page  */}
+        <Route path='/' element={<HomePageComponent/>} />
+        <Route path='/profile' element={<ProfilePageComponent/>} />
+      </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/Components/HomePageComponent.js
+++ src/Components/HomePageComponent.js
@@ -1,10 +1,21 @@
 //rafce short hand for react arrow function component export
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 const HomePageComponent = () => {
+
+    const navigate = useNavigate(); //Reacts-router-dom's method for changing URL locations
+
+    const handlePageChange = () => {
+        navigate('/profile');
+    }
+
   return (
     <div>
       <h1>Hello this is our Home Page</h1>
+
+      <Button variant="secondary" onClick={() => handlePageChange()}>Go To Profile Page</Button>{' '}
     </div>
   )
 }
