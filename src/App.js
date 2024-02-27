87629a4a7c40d407ef236a0838d7a2c0546adec5
JayvonK <jaykhuth@gmail.com>
2024-02-27 09:22:13 -0800

    usercontext

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePageComponent from './Components/HomePageComponent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProfilePageComponent from './Components/ProfilePageComponent';
+import UserContext from './UserContext/UserContext';
 
 function App() {
   return (
+    <UserContext>
       <BrowserRouter>
-      {/* Wrapping our application in browserRouter to enable Routing */}
+        {/* Wrapping our application in browserRouter to enable Routing */}
 
-      <Routes>
-        {/* '/' deteramnds our hompage our are default rendered page  */}
-        <Route path='/' element={<HomePageComponent/>} />
-        <Route path='/profile' element={<ProfilePageComponent/>} />
-      </Routes>
+        <Routes>
+          {/* '/' deteramnds our hompage our are default rendered page  */}
+          <Route path='/' element={<HomePageComponent />} />
+          <Route path='/profile' element={<ProfilePageComponent />} />
+        </Routes>
       </BrowserRouter>
+    </UserContext>
+
   );
 }
 
